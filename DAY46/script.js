10f89a3cd56c2be69314edd807a48e0899b3658c
maabcodingschool <82595824+maabcodingschool@users.noreply.github.com>
2021-05-29 20:07:14 +0500

    Commit
    Commit

---
+++ DAY46/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: "Lanborghini qayerning mashinasi",
+        a: "Italiya",
+        b: "Argentina",
+        c: "Europe",
+        d: "Paris",
+        correct: "a",
+        },
+        {
+        question: "Rim papsining Lamborghini mashinasni qanchaga sotildi",
+        a: "715 ming dollorga",
+        b: "715 ming yevroga",
+        c: "816 ming dollorga",
+        d: "648 ming yevroga",
+        correct: "b",
+        },
+        {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "c",
+        },
+        {
+        question: "",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "d",
+        },
+];
