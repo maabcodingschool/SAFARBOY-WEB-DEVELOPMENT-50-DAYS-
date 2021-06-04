e51afb4f49bc43b026220a738c62d4a4cd106023
maabcodingschool <82595824+maabcodingschool@users.noreply.github.com>
2021-06-04 20:31:57 +0500

    Commit
    Commit

---
+++ DAY 9/script.js
@@ -0,0 +1,25 @@
+const sounds = ['Callin', 'Perfect', 'Niva', 'Rainy Day', 'When I Win', 'Udar bass']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+
+}
